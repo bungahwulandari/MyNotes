11f95497e004c6e6b54919790ce2b935a04f9c29
bungahwulandari <bungahwlndr@gmail.com>
2026-01-31 01:50:37 +0700

    Create NoteDao.kt

---
+++ NoteDao.kt
@@ -0,0 +1,19 @@
+package com.example.mynotes
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    suspend fun insert(note: Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    suspend fun getAllNotes(): List<Note>
+}
